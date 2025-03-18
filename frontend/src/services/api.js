5202e750398ea11e1d5c20b1827fb48abd6e73ea
Kamlesh Prajapati <kamleshprajapati0928@gmail.com>
2025-03-18 13:29:07 +0530

    Added deployed link of backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:7000/api";
+const API_BASE_URL = "https://eco-track-mvp-backend.onrender.com/api";
 
 export const fetchWeatherData = async (city) => {
     try {
