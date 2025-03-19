1701a4703065f55e33bb3b5fda1ce1ed7337eda5
Kamlesh Prajapati <kamleshprajapati0928@gmail.com>
2025-03-19 17:31:54 +0530

    Added updated links

--- frontend/src/services/handleGoogleAuth.js
+++ frontend/src/services/handleGoogleAuth.js
@@ -24,16 +24,16 @@ const handleGoogleAuth = async () => {
         createdAt: new Date(),
       });
 
-      window.location.href = "/location";
+      window.location.href = "https://eco-track-demo.onrender.com/location";
     } else {
       // Check if location data exists
       const userData = userDoc.data();
       if (userData.location) {
         // Location data exists, redirect to /home
-        window.location.href = "/home";
+        window.location.href = "https://eco-track-demo.onrender.com/home";
       } else {
         // Location data doesn't exist, redirect to /location
-        window.location.href = "/location";
+        window.location.href = "https://eco-track-demo.onrender.com/location";
       }
     }
 
